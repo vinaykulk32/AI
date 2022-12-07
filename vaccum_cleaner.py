8b41e7a7c9f50c45d665cf73f967702bc2e098e9
vinay <84218725+vinaykulk32@users.noreply.github.com>
2022-12-07 10:46:50 +0530

    Update vaccum_cleaner.py

--- vaccum_cleaner.py
+++ vaccum_cleaner.py
@@ -1,26 +1,31 @@
-def clean(floor, goal):
+def clean(floor, goal, cost):
     for i in range(3):
         if floor == goal:
             print("attained goal state")
-            print_floor(floor)
+            print_floor(floor, cost)
             return
         if floor[i] == goal[i]:
             continue
         if floor[i] == 1:
             print("STATUS:DIRTY")
-            print_floor(floor)
+            print("room"+str(i+1))
+            print_floor(floor, cost)
             floor[i] = 0
+            cost += 1
+            print("moving to room"+str(i+2))
         else:
             print("STATUS:CLEAN")
-            print_floor(floor)
+            print_floor(floor, cost)
 
 
-def print_floor(floor):
+def print_floor(floor, cost):
     print(floor)
+    print("cost:", cost)
     print("-----------------")
 
 
 # Test 1
-room = [0, 0]
+room = [1, 1]
 goal = [0, 0]
-clean(room, goal)
+cost = 0
+clean(room, goal, cost)
